602eabe678e592d9886d142d08344a66781f6196
Hamza Ali <hamxa1331@gmail.com>
2018-09-05 02:56:06 +0500

    Logout Implemented
    Logout and session routes implemented

--- server/routes/api/Signin.js
+++ server/routes/api/Signin.js
@@ -12,16 +12,16 @@ module.exports = (app) => {
         let {
             firstName,lastName,email,password
         } = body
-        if(!firstName){return res.send({succes:false,message:"User must must have First Name"})}
-        if(!email){return res.send({succes:false,message:"User must must have Email"})}
-        if(!password){return res.send({succes:false,message:"User must must have Password"})}
-        if(!lastName){return res.send({succes:false,message:"User must must have Last Name"})}
+        if(!firstName){return res.send({success:false,message:"User must must have First Name"})}
+        if(!email){return res.send({success:false,message:"User must must have Email"})}
+        if(!password){return res.send({success:false,message:"User must must have Password"})}
+        if(!lastName){return res.send({success:false,message:"User must must have Last Name"})}
         email = email.toLowerCase()
         User.find({
             email:email
         },(err,prevUser)=>{
-            if(err){return res.send({succes:false,message:"Internal Server Error"})}
-            else if(prevUser.length>0){return res.send({succes:false,message:"User already exist"})}
+            if(err){return res.send({success:false,message:"Internal Server Error"})}
+            else if(prevUser.length>0){return res.send({success:false,message:"User already exist"})}
             
             let newUser = new User();
             newUser.email = email;
@@ -30,7 +30,7 @@ module.exports = (app) => {
             newUser.password = newUser.generateHash(password)
             newUser.save((err,user)=>{
                 if(err){
-                    return res.send({succes:false,message:"Internal Server Error"})
+                    return res.send({success:false,message:"Internal Server Error"})
                 }
                 return res.send(user)
             })
@@ -42,27 +42,27 @@ module.exports = (app) => {
           email,password
       } = body
       email = email.toLowerCase()
-      if(!email){return res.send({succes:false,message:"User must must have an Email"})};
-      if(!password){return res.send({succes:false,message:"User must must have a Password"})}
+      if(!email){return res.send({success:false,message:"User must must have an Email"})};
+      if(!password){return res.send({success:false,message:"User must must have a Password"})}
       User.find({email:email},(err,users)=>{
           if(err){
-              return res.send({succes:false,message:'Internal Server Error'})
+              return res.send({success:false,message:'Internal Server Error'})
           }
           if(users.length!=1){
-              return res.send({succes:false,message:'Invalid Login'})
+              return res.send({success:false,message:'Invalid Login'})
           }
           const user = users[0]
           if(!user.validPassword(password)){
-              return res.send({succes:false,message:'Invalid Password'})
+              return res.send({success:false,message:'Invalid Password'})
           }
           let userSession = new UserSession()
-          userSession.id = user._id
+          userSession.userId = user._id
           userSession.save((err,doc)=>{
               if(err){
-                  return res.send({succes:false,message:'Internal Server Error'})
+                  return res.send({success:false,message:'Internal Server Error'})
               }
               return res.send({
-                  succes:true,
+                  success:true,
                   message:'Sign in Succesfully',
                   token:doc._id
               })
@@ -72,4 +72,37 @@ module.exports = (app) => {
       })
   
   })
+  app.get('/api/accounts/verify/:token',(req,res)=>{
+    const {token} = req.params
+    UserSession.findById(token,(err,session)=>{
+        if(err){
+            return res.send({success:false,message:"Internal Server Error"})
+        }
+        if(!session.isDeleted)
+            return res.send({
+                success:true,
+                message:'Good'
+            })
+        else
+        return res.send({success:false,message:"Session deleted"})
+        
+    })
+})
+app.post('/api/accounts/logout',(req,res)=>{
+    const {token} = req.body
+    // UserSession.findByIdAndUpdate(token,$set({isDeleted:true}),null,(err,session)=>{
+    //     if(err){
+    //         return res.send({success:false,message:"Internal Server Error"})
+    //     }
+    //     return res.send({success:true,message:"Session deleted"})
+        
+    // })
+    UserSession.findOneAndUpdate({userId:token}, { $set: { isDeleted: true }}, { new: true },  (err, session)=>{
+            if(err){
+            console.error(err)
+            return res.send({success:false,message:"Internal Server Error"})
+        }
+        return res.send({success:true,message:"Session deleted"})
+    })
+})
   };
