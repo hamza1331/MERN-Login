42acb1e45eaea85f26810422582dd7a502a0d1dd
Hamza Ali <hamxa1331@gmail.com>
2018-09-04 11:07:04 +0500

    Signin Implemented
    Signin route and feature implemented on the server.

--- server/models/User.js
+++ server/models/User.js
@@ -26,6 +26,6 @@ UserSchema.methods.generateHash = function(password){
     return bcrypt.hashSync(password,bcrypt.genSaltSync(8),null)
 }
 UserSchema.methods.validPassword = function(password){
-    return bcrypt.compareSync(password.this.password)
+    return bcrypt.compareSync(password,this.password)
 }
 module.exports = mongoose.model('User', UserSchema);
--- server/routes/api/Signin.js
+++ server/routes/api/Signin.js
@@ -1,4 +1,5 @@
 const User = require('../../models/User')
+const UserSession = require('../../models/UserSession')
 module.exports = (app) => {
     // app.get('/api/counters', (req, res, next) => {
     //   Counter.find()
@@ -22,7 +23,7 @@ module.exports = (app) => {
             if(err){return res.send({succes:false,message:"Internal Server Error"})}
             else if(prevUser.length>0){return res.send({succes:false,message:"User already exist"})}
             
-            const newUser = new User();
+            let newUser = new User();
             newUser.email = email;
             newUser.firstName = firstName;
             newUser.lastName = lastName;
@@ -35,4 +36,40 @@ module.exports = (app) => {
             })
         })
     })
+    app.post('/api/accounts/signin',(req,res)=>{
+      const {body} = req
+      let {
+          email,password
+      } = body
+      email = email.toLowerCase()
+      if(!email){return res.send({succes:false,message:"User must must have an Email"})};
+      if(!password){return res.send({succes:false,message:"User must must have a Password"})}
+      User.find({email:email},(err,users)=>{
+          if(err){
+              return res.send({succes:false,message:'Internal Server Error'})
+          }
+          if(users.length!=1){
+              return res.send({succes:false,message:'Invalid Login'})
+          }
+          const user = users[0]
+          if(!user.validPassword(password)){
+              return res.send({succes:false,message:'Invalid Password'})
+          }
+          let userSession = new UserSession()
+          userSession.id = user._id
+          userSession.save((err,doc)=>{
+              if(err){
+                  return res.send({succes:false,message:'Internal Server Error'})
+              }
+              return res.send({
+                  succes:true,
+                  message:'Sign in Succesfully',
+                  token:doc._id
+              })
+          })
+  
+  
+      })
+  
+  })
   };
